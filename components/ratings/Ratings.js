f3f4c630286bdc938bdc68138a826644387c55b3
Nana <you@example.com>
2019-09-18 21:50:25 +0800

    筛选评价

--- components/ratings/Ratings.js
+++ components/ratings/Ratings.js
@@ -12,13 +12,26 @@ Component({
     * 组件的初始数据
     */
    data: {
-
+      selectType: 1
    },
 
    /**
     * 组件的方法列表
     */
    methods: {
-
+      filterRatings: function(e){
+         let ratings = this.properties.ratings
+         let selectType = e.target.dataset.index
+         if(selectType == 0 || selectType == 1){
+            var a = ratings.filter(item => {
+               return item.rateType == selectType
+            });
+         }
+         
+         this.setData({
+            selectType
+         })
+         console.log(a)
+      }
    }
 })
